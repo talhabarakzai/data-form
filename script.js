7c95a5be8510fdadbb5205d4ab6a4384dc868322
talhabarakzai <talhabarakzai123@gmail.com>
2024-08-05 20:48:08 +0500

    new web

---
+++ script.js
@@ -0,0 +1,4 @@
+let firstName = prompt("write your first name")
+let LastName = prompt("write your last name")
+let age = prompt("write your age")
+alert("Make Sure Your Given Data Is Correct:\n"+ "Name :"+  " " + firstName + " " + LastName + " \n"+ "Age :"+ " " + age)
